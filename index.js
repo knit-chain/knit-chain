cf647729d205d8f9fe6de9e188f6146b68ecd8ce
Ender Smith <Ender Smith>
2018-03-15 14:09:19 -0700

    added simple mining, difficulty, hashing, and validating

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 'use strict';
 
+require('dotenv').config();
 const KC = require('./lib/kc');
+
 const myKnitChain = new KC();
 
 console.log(`DEMO START`);
 
 console.log(`\ncreate new knitChain:\nmyKnitChain: ${JSON.stringify(myKnitChain)}`);
 
-console.log(`\ninsert document with value 90:\nlatest block: ${JSON.stringify(myKnitChain.insert(90))}`);
+console.log(`\ninsert document with value 90:\nlatest block: ${JSON.stringify(myKnitChain.insertData(90))}`);
 
-console.log(`\ninsert document with value 81:\nlatest block: ${JSON.stringify(myKnitChain.insert(81))}`);
+console.log(`\nlookup chain: ${JSON.stringify(myKnitChain.chain)}`);
 
-console.log(`\n(the first block is now full)`);
-
-console.log(`\nlookup by latest.hash:\nhashArray[${myKnitChain.latest.hash}]: ${JSON.stringify(myKnitChain.hashArray[myKnitChain.latest.hash])}`);
-
-console.log(`\ninsert document with value 72:\nlatest block: ${JSON.stringify(myKnitChain.insert(72))}`);
-
-console.log(`\nlookup by latest.previousHash:\nhashArray[${myKnitChain.latest.previousHash}]: ${JSON.stringify(myKnitChain.hashArray[myKnitChain.latest.previousHash])}`);
-
-console.log(`\ninsert document with value 63:\nlatest block: ${JSON.stringify(myKnitChain.insert(63))}`);
+console.log(`\ninsert document with value 81:\nlatest block: ${JSON.stringify(myKnitChain.insertData(81))}`);
 
 console.log(`\n(the first block is now full)`);
+
+console.log(`\nlookup chain: ${JSON.stringify(myKnitChain.chain)}`);
+
+console.log(`\ninsert document with value 72:\nlatest block: ${JSON.stringify(myKnitChain.insertData(72))}`);
+
+console.log(`\nlookup chain: ${JSON.stringify(myKnitChain.chain)}`);
+
+console.log(`\ninsert document with value 63:\nlatest block: ${JSON.stringify(myKnitChain.insertData(63))}`);
+
+console.log(`\n(the second block is now full)`);
+
+console.log(`\nlookup chain: ${JSON.stringify(myKnitChain.chain)}`);
+
+console.log(`\ngetLatest: ${JSON.stringify(myKnitChain.getLatest())}`);
+
+console.log(`\nisValid: ${myKnitChain.isValid()}`);
 
 console.log(`\nEND OF DEMO\n`);
